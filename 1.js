dc7d1689808ac34641bd7c3d0544e98c43c24d48
RikyShafer <r8524409@gmail.com>
2024-04-02 15:28:34 +0300

    riki_2! pr 2

--- 1.js
+++ 1.js
@@ -1,10 +1,19 @@
 alert("היי")
 const r=()=>{
-    alert(5)
+    aconsole.log("r");
+    alert("r")
 }
 
 
 const i=()=>{
+    console.log("i");
+    alert("i")
+}
+const k=()=>{
+    console.log("k");
+    alert("k")
+}
+const riki=()=>{
     console.log("riki");
     alert("riki")
 }
